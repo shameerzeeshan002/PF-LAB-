7d53dd2cae770f1e50e4bcb5832f4484e9faa908
shameerzeeshan002 <shameerzeeshan002@gmail.com>
2026-02-20 01:31:50 +0500

    circle

---
+++ week 2/AKZKS.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout << " ####" << endl;
+    cout << "##  ##" << endl;
+    cout << "######" << endl;
+    cout << "##  ##" << endl;
+    cout << "##  ##" << endl;
+
+    cout << endl;
+
+    cout << "##  ##" << endl;
+    cout << "##  ##" << endl;
+    cout << "####" << endl;
+    cout << "##" << endl;
+    cout << "####" << endl;
+    cout << "##  ##" << endl;
+    cout << "##  ##" << endl;
+
+    cout << endl;
+
+    cout << "######" << endl;
+    cout << "    ##" << endl;
+    cout << "   ##" << endl;
+    cout << "  ##" << endl;
+    cout << " ##" << endl;
+    cout << "######" << endl;
+
+    cout << endl;
+
+    cout << "##  ##" << endl;
+    cout << "##  ##" << endl;
+    cout << "#####" << endl;
+    cout << "####" << endl;
+    cout << "#####" << endl;
+    cout << "##  ##" << endl;
+    cout << "##  ##" << endl;
+
+    cout << endl;
+
+    cout << " ####" << endl;
+    cout << "##  ##" << endl;
+    cout << "##" << endl;
+    cout << " ####" << endl;
+    cout << "    ##" << endl;
+    cout << "##  ##" << endl;
+    cout << " ####" << endl;
+
+}
