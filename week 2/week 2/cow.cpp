edb67b8d52d9dfd09b0a49c3f5885e754debfe2c
shameerzeeshan002 <shameerzeeshan002@gmail.com>
2026-03-01 23:30:45 +0500

    pf lab

---
+++ week 2/week 2/cow.cpp
@@ -0,0 +1,26 @@
+#iclude<iostream>
+using namespace std;
+main()
+{
+cout<<"       ----------------------------------"<<endl;
+cout<< ""<<endl;
+cout<< ""<<endl;
+cout<< "          o  "<<endl;
+cout<< "               ----------------:.           "<<endl;
+cout<< "              :-----------::.                "<<endl;
+cout<<"               -----------:.                 "<<endl;
+cout<<"               :-------------:..                "<<endl;
+cout<<"                :----------------:.             "<<endl;
+cout<<"                 .---------------:            "<<endl;
+cout<<"                   .:---------:.             "<<endl;
+
+
+
+
+
+
+
+
+
+
+}
